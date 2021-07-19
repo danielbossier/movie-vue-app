3631700b05ad3c293e53fedae3479b3bef47d03b
danielbossier <dbossier21@gmail.com>
2021-07-19 15:56:53 -0700

    submit HW

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2>Add a flick!</h2>
+    <button v-on:click="createMovie()">Create Movie</button>
+    <h1>{{ message }}</h1>
+    <div v-for="movie in movies" :key="movie.id">
+      <h2>Title: {{ movie.title }}</h2>
+      <h2>Plot: {{ movie.plot }}</h2>
+    </div>
   </div>
 </template>
-
+<style></style>
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Watch these movies, sucka!",
+      movies: [],
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("http://localhost:3000/movies").then((response) => {
+        this.movies = response.data;
+        console.log("All Movies:", this.movies);
+      });
+    },
+    createMovie: function () {
+      console.log("added that movie, baybay!");
+
+      var params = {
+        title: "A Clockwork Orange",
+        year: 1971,
+        plot: "Alex DeLarge has a thirst for milk and a bit of old ultra-violence.",
+        director: "Stanley Kubrick",
+      };
+      axios
+        .post("http://localhost:3000/movies", params)
+        .then((response) => {
+          console.log("Success!", response.data);
+          this.movies.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
   },
 };
 </script>
